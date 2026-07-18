f62f31ba5e6352b6d62c9d6e47dc889212f1080c
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2026-07-18 17:05:51 +0530

    Day 2 : Variables and data types

---
+++ DataType.java
@@ -0,0 +1,76 @@
+
+public class DataType {
+    public static void main(String[] args) {
+
+        // Variable is a named memory location used to store data, this is stored in RAM during program execution.
+
+        // Data Types - Primitive Data Types - Non-Primitive Data Types
+
+        // Primitive Data Types (Built-in)
+
+        // Primitive variables store the ACTUAL VALUE.
+        // There are 8 Primitive Data Types.
+
+        // byte -> 1 byte
+        // short -> 2 bytes
+        // int -> 4 bytes
+        // long -> 8 bytes
+        // float -> 4 bytes
+        // double -> 8 bytes
+        // char -> 2 bytes (Unicode)
+        // boolean -> true / false (JVM dependent)
+
+        byte b = 100;
+        short s = 1000;
+        int age = 21;
+        long population = 8000000000L;
+        float pi = 3.14f;
+        double cgpa = 8.35678;
+        char grade = 'A';
+        boolean isPlaced = false;
+
+        // Primitive variables are generally stored in Stack Memory.
+       
+
+        // Non-Primitive
+        // Examples: String, Array, Class, Object
+
+        // Non-Primitive variables DO NOT store the actual object.
+        // They store the REFERENCE (memory address) of the object.
+
+        // Array
+
+        int[] nums = new int[5];
+
+        // int vs Integer
+        // int -> Primitive Data Type
+        // Integer -> Wrapper Class (Object)
+        //
+        // int age = 21;
+        //
+        // Integer ageObj = 21; // Auto Boxing
+
+        // Default Primitive Values (for class variables)
+        //
+        // byte -> 0
+        // short -> 0
+        // int -> 0
+        // long -> 0L
+        // float -> 0.0f
+        // double -> 0.0
+        // char -> '\u0000'
+        // boolean -> false
+
+        // Variable Declaration
+
+        int marks;
+
+        // Initialization
+
+        marks = 95;
+
+        // Declaration + Initialization
+
+        int score = 100;
+    }
+}
