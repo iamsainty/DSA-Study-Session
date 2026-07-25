3a0f90c5cab75ad1bfc49a775ba08d82872507f8
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2026-07-25 11:06:07 +0530

    Day 7 : Arrays

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int[] arr = new int[100]; 
+        int[] nums = new int[100]; 
+
+        System.out.println(Arrays.equals(arr, nums));
+
+        
+
+    }
+}
